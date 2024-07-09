f780b4b950464b47ce78e3c8648c961bffca5aee
mehed1164 <md.hasan1164@gmail.com>
2024-07-09 12:50:14 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Graphics Designer", "Visual Editor","UI/UX Designer","Motion Graphics Designer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
